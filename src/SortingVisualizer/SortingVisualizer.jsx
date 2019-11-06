f9dc11c250bfde94faef59c1f1e89291738c7465
cfowler650 <cfowler650@gmail.com>
2019-11-06 09:19:35 -0600

    sorting visualizer component

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,2 +1,46 @@
 import React from 'react';
 import './SortingVisualizer.css';
+
+
+
+export default class SortingVisualizer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            array: []
+        }
+    }
+
+    componentDidMount() {
+        this.resetArray();
+    }
+
+    resetArray() {
+        const array = [];
+        for (let i = 0; i < 100; i++) {
+            array.push(randomIntFromInterval(5, 1000));
+        }
+        this.setState({ array });
+    }
+
+    render() {
+        const { array } = this.state;
+
+        return (
+            <>
+                {array.map((value, idx) => {
+                    <div className="array-bar" key={idx}>
+                        {value}
+                    </div>
+                })}
+            </>
+        );
+    }
+}
+
+//from https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+function randomIntFromInterval(min, max) {
+    //min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
